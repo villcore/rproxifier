5cd9378ef02835ce4eee1b597e2e4182854c3971
villcore <villcore@163.com>
2022-01-25 22:40:44 +0800

    Add windows tun config.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,44 @@
 use std::io::Read;
-use std::thread::sleep;
+use std::thread::{sleep, spawn};
 use std::time::Duration;
 
+use std::process::{Command, exit};
+use std::net::{IpAddr, Ipv4Addr};
+use async_std_resolver::{resolver, config, AsyncStdResolver};
+use crate::dns::resolve::{ForwardingDnsResolver, DirectDnsResolver, UserConfigDnsWrapResolver};
+use crate::dns::server::DnsUdpServer;
+use smoltcp::wire::{IpAddress, IpProtocol, Ipv4Cidr, Ipv4Packet, TcpPacket, UdpPacket};
+
+use log::{info, error};
+use std::collections::HashMap;
+// use crate::sys::darwin::setup_ip;
+
 fn main() {
-    println!("Hello, world!");
-    run_macos();
-    run_windows();
+    setup_log();
+    info!("Hello, world!");
+    // run_macos();
+
+    let run_time = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let dns_listen = "";
+    // let dns_setup = sys::darwin::DNSSetup::new(dns_listen.to_string());
+
+    // start tun_server
+    let tun_server_join_handle = spawn(|| start_tun_server());
+
+    // run dns server
+    run_time.block_on(start_user_config_dns_server());
+
+    tun_server_join_handle.join();
+    info!("Stop.");
+    exit(0)
+}
+
+fn setup_log() {
+    log4rs::init_file("config/logrs.yaml", Default::default()).unwrap();
 }
 
 fn run_macos() {
@@ -28,5 +61,78 @@ fn run_windows() {
     // TODO: setup route
     // TODO: start read & write
     // TODO: notice direct
-    sleep(Duration::from_secs(100))
+}
+
+async fn start_user_config_dns_server() {
+    info!("start dns server");
+    let resolver_config = config::ResolverConfig::default();
+    let resolver_opts = config::ResolverOpts::default();
+    let resolver = resolver(resolver_config, resolver_opts).await
+        .expect("failed to connect resolver");
+
+    let dns_listen = "127.0.0.1:53";
+    let direct_dns_resolver = DirectDnsResolver::new(resolver);
+    let mut domain_map = HashMap::new();
+    domain_map.insert("www.bing.com".to_string(), Ipv4Addr::new(18, 0, 0, 10));
+    let user_config_dns_resolver = UserConfigDnsWrapResolver::new(domain_map, Box::new(direct_dns_resolver));
+    let dns_server: DnsUdpServer = dns::server::DnsUdpServer::new(
+        dns_listen.to_string(),
+        Box::new(user_config_dns_resolver)
+    ).await;
+    dns_server.run_server().await;
+    info!("stop dns server");
+}
+
+fn start_tun_server() {
+
+    // TODO: use common config
+    let tun_ip = "18.0.0.1";
+    let tun_cidr = "18.0.0.0/16";
+    let netmask = "255.255.255.0";
+    let tun_name = "utun9";
+
+    //
+    //
+    info!("start tun {}", tun_name);
+
+    // // config route
+    // setup_ip(tun_name, tun_ip, tun_cidr);
+
+    //
+    // start tun handle
+    // let mut buf = [0u8; 2048];
+    // loop {
+    //     let read_size = tun_socket.read(&mut buf).unwrap();
+    //     if read_size == 0 {
+    //         log::error!("tun read error");
+    //         break;
+    //     }
+    //
+    //     let mut ipv4_packet = match Ipv4Packet::new_checked(&buf[..read_size]) {
+    //         Ok(p) => {
+    //             p
+    //         }
+    //         Err(_) => {
+    //             tracing::error!("tun read ip_v4 packet error");
+    //             continue;
+    //         }
+    //     };
+    //
+    //     // modify ipv4 packet address & port
+    //     println!("Ip protocol = {}, src = {}, dst = {}", ipv4_packet.protocol().to_string(), ipv4_packet.src_addr(), ipv4_packet.dst_addr());
+    //     match ipv4_packet.protocol() {
+    //         IpProtocol::Tcp => {
+    //             let tcp_packet = TcpPacket::new_checked(ipv4_packet.payload()).unwrap();
+    //             log::info!("tun read tcp package from src {} src_port {} ", ipv4_packet.src_addr(), tcp_packet.src_port());
+    //
+    //             // TODO: modify
+    //         }
+    //         // IpProtocol::Udp => {
+    //         //
+    //         // }
+    //         other => {
+    //             log::error!("unsupported ipv4 protocol {} ", other);
+    //         }
+    //     }
+    //     // device.write(&buf[..read_size]);
 }
